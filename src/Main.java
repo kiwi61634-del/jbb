ab536d288c6cf34bbbe818721882d54e49dc4a45
kiwi61634-del <kiwi61634@gmail.com>
2025-11-27 10:37:14 +0900

    객체 생성시 id 증가 --> 연습한다

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1");
+        System.out.println(new Article().id);
+        System.out.println(new Article().id);
+        System.out.println(new Article().id);
+        System.out.println(new Article().id);
+        System.out.println(new Article().id);
+        System.out.println(new Article().id);
+        System.out.println(new Article().id);
+        System.out.println(new Article().id);
+        System.out.println(new Article().id);
+        System.out.println(new Article().id);
+    }
+}
+
+class Article {
+
+    static int lastId;
+    int id;
+
+    static {
+        lastId = 0;
+    }
+    Article(){
+        this(lastId + 1);
+        lastId++;
+    }
+    Article(int id){
+        this.id =id;
     }
 }
