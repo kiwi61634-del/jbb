b0a1c081200b04adeaca5c4b3bf2f72711afb1eb
kiwi61634-del <kiwi61634@gmail.com>
2025-12-01 16:24:59 +0900

    article write 구현

--- src/com/woori/BAM/Main.java
+++ src/com/woori/BAM/Main.java
@@ -1,42 +1,68 @@
 package com.woori.BAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("== 프로그램 시작 ==");
         Scanner sc = new Scanner(System.in);
-        int id = 1;  //게시글의 번호를 저장
+
+        int id = 1;
+
+        List<Article> articles = new ArrayList<>();
+
         while (true) {
             System.out.print("명령어) ");
-            String cmd = sc.nextLine().trim();   //trim() 공부 --> 사용
+            String cmd = sc.nextLine().trim();
 
             if ( cmd.length() == 0) {
                 System.out.println("명령어를 입력해 주세요");
-                continue;        // 반복문 처음부터 다시 진행
+                continue;
             }
 
+
             if ( cmd.equals("article list")) {
-                System.out.println("게시글이 없습니다");
-            } else if (cmd.equals("article write")) {
+               if (articles.size()==0){
+                   System.out.println("계시글이 없습니다");
+                   continue;
+               }
+
+                System.out.println("번호  |  제목");
+               for (int i = 0; i < articles.size(); i++){
+                   Article article=articles.get(i);
+                   System.out.printf("%d  |  %s \n" , article.id, article.title);
+                   continue;
+               }
+
+            }else if (cmd.equals("article write")) {
                 System.out.print("제목 : ");
                 String title = sc.nextLine();
                 System.out.print("내용 : ");
                 String body = sc.nextLine();
-                System.out.println(id + " 번글이 생성되었습니다");
+                System.out.println(id + "번글이 생성되었습니다");
+
+                Article article= new Article();
+                article.id = id;
+                article.title= title;
+                article.body= body;
+
+                articles.add(article);
                 id++;
+
             } else {
                 System.out.println("존재하지 않는 명령어 입니다");
             }
-
-            if (cmd.equals("exit")) { //cmd 객체의 값이 "exit" 같은지
-                break;                //반복문을 빠져나감
+            if (cmd.equals("exit")) {
+                break;
             }
-
-
         }
-
         System.out.println("== 프로그램 종료 ==");
     }
-
+}
+class Article{
+    int id;
+    String title;
+    String body;
 }
