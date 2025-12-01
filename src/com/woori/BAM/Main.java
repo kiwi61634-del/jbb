6ff5fdabf8cb0b9095d53bab04679be9ba671c8b
kiwi61634-del <kiwi61634@gmail.com>
2025-12-01 11:17:45 +0900

    무한루프

--- src/com/woori/BAM/Main.java
+++ src/com/woori/BAM/Main.java
@@ -4,10 +4,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("== 프로그램 시작 ==");
-        Scanner sc = new Scanner(System.in);
-        System.out.print("cmd) ");
-        System.out.println("명령어) "+sc.nextLine() );
+            System.out.println("== 프로그램 시작 ==");
+        while(true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("cmd) ");
+            String cmd = sc.nextLine();
+            System.out.println("명령어) " + cmd);
+            if (cmd.equals("exit")) {
+                break;
+            }
+        }
         System.out.println("== 프로그램 종료 ==");
 
 
