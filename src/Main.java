2944e21b8f1cf19b3dc9594bdb69043589eb5796
kiwi61634-del <kiwi61634@gmail.com>
2025-12-01 09:36:28 +0900

    자바게시판 연습

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.List;       // Ctrl + Alt + L --> 자동 import , 포맷 재구성
 
-public class Main {
+class Main {
     public static void main(String[] args) {
 //        exam01();
 //        exam02();
@@ -10,8 +10,8 @@ public class Main {
     }
 
 
-    static void exam01()  {   //articleLength 변수 생성, for문을 이용해서 NPE 회피 --> 리모델링 필요
-        int articleLength = 0 ;
+    static void exam01() {   //articleLength 변수 생성, for문을 이용해서 NPE 회피 --> 리모델링 필요
+        int articleLength = 0;
         Article[] articles = new Article[10];
 
         articles[0] = new Article();
@@ -27,7 +27,7 @@ public class Main {
         articles[5] = new Article();
         articleLength++;
 
-        for(int i = 0 ; i < articleLength; i++ ) {
+        for (int i = 0; i < articleLength; i++) {
             Article article = articles[i];
             System.out.println(article.id);
         }
@@ -49,8 +49,8 @@ public class Main {
 
         System.out.println(articles);  // toString() 호출 , List는 [o , o, o] 형태로 출력됨
 
-        for (int i =0 ; i < articles.size() ; i++ ) {   // size() --> ArrayList 타입의 배열의 크기를 가져옴
-            Article article =(Article) articles.get(i);  //강제 형변환,   get(i)는 return 타입이 Object(모든 class 부모)
+        for (int i = 0; i < articles.size(); i++) {   // size() --> ArrayList 타입의 배열의 크기를 가져옴
+            Article article = (Article) articles.get(i);  //강제 형변환,   get(i)는 return 타입이 Object(모든 class 부모)
             System.out.println();
         }
     }
@@ -63,7 +63,7 @@ public class Main {
         articles.add(new Article());
         articles.add(new Article());
 
-        for(Article article : articles) {   //향상된 for문도 잘 수행
+        for (Article article : articles) {   //향상된 for문도 잘 수행
             System.out.println(article.id);
         }
 
@@ -73,14 +73,28 @@ public class Main {
 //        }
 
     }
-    static void exam04(){
-        List<Article> articles = new ArrayList<>();  //제너릭 사용 --> 'E' Article 지정       articles.add(new Article());
+
+    static void exam04() {     // List,  ArrayList 사용함
+        List<Article> articles = new ArrayList<>();
         articles.add(new Article());
         articles.add(new Article());
         articles.add(new Article());
         articles.add(new Article());
         articles.add(new Article());
+
+        for (Article article : articles) {   //향상된 for문도 잘 수행
+            System.out.println(article.id);
+        }
+
+//        for (int i =0 ; i < articles.size() ; i++ ) {
+//            Article article = articles.get(i);    // 강제형변환을 안해도 된다
+//            System.out.println(article.id);
+//        }
+
     }
+
+
+
 
 }
 
---
+++ src/com/woori/BAM/Main.java
@@ -0,0 +1,15 @@
+package com.woori.BAM;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("== 프로그램 시작 ==");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("cmd) ");
+        System.out.println("명령어) "+sc.nextLine() );
+        System.out.println("== 프로그램 종료 ==");
+
+
+    }
+}
