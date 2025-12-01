b038172dd63b2ea1edb72d6dacccd5238b7164d8
kiwi61634-del <kiwi61634@gmail.com>
2025-12-01 17:57:03 +0900

    article detail 구현중

--- src/com/woori/BAM/Main.java
+++ src/com/woori/BAM/Main.java
@@ -17,52 +17,60 @@ public class Main {
             System.out.print("명령어) ");
             String cmd = sc.nextLine().trim();
 
-            if ( cmd.length() == 0) {
+            if (cmd.length() == 0) {
                 System.out.println("명령어를 입력해 주세요");
                 continue;
             }
 
 
-            if ( cmd.equals("article list")) {
-               if (articles.size()==0){
-                   System.out.println("계시글이 없습니다");
-                   continue;
-               }
+            if (cmd.equals("article list")) {
+                if (articles.size() == 0) {
+                    System.out.println("계시글이 없습니다");
+                    continue;
+                }
 
                 System.out.println("번호  |  제목");
-               for (int i = 0; i < articles.size(); i++){
-                   Article article=articles.get(i);
-                   System.out.printf("%d  |  %s \n" , article.id, article.title);
-                   continue;
-               }
+                for (int i = 0; i < articles.size(); i++) {
+                    Article article = articles.get(i);
+                    System.out.printf("%d  |  %s \n", article.id, article.title);
+                    continue;
+                }
 
-            }else if (cmd.equals("article write")) {
+            } else if (cmd.equals("article write")) {
                 System.out.print("제목 : ");
                 String title = sc.nextLine();
                 System.out.print("내용 : ");
                 String body = sc.nextLine();
                 System.out.println(id + "번글이 생성되었습니다");
 
-                Article article= new Article();
+                Article article = new Article();
                 article.id = id;
-                article.title= title;
-                article.body= body;
+                article.title = title;
+                article.body = body;
 
                 articles.add(article);
                 id++;
+            } else if (cmd.startsWith("article detail")){
+                String[] cmdBits = cmd.split(" ");
+                System.out.println(cmdBits[0]);
+                System.out.println(cmdBits[1]);
+                System.out.println(cmdBits[2]);
 
             } else {
                 System.out.println("존재하지 않는 명령어 입니다");
             }
+
             if (cmd.equals("exit")) {
-                break;
-            }
+                    break;
+                }
         }
-        System.out.println("== 프로그램 종료 ==");
+            System.out.println("== 프로그램 종료 ==");
     }
-}
-class Article{
-    int id;
-    String title;
-    String body;
+
+    static class Article {
+
+        int id;
+        String title;
+        String body;
+    }
 }
